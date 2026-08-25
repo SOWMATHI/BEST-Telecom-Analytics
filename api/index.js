d0872c497aff2d6effde0f20a5ce6fbd5cb8e274
SOWMATHI <rsowmathi22@gmail.com>
2026-08-25 12:43:44 +0530

    chore: remove root configuration and Vercel deployment files

--- api/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require('../backend/src/app');
-const connectDB = require('../backend/src/config/db');
-
-// Connect to MongoDB
-connectDB();
-
-// Export the Express API
-module.exports = app;
